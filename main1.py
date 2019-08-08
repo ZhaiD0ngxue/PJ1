401f7a7dc4c7a2328e649e6d0351c04377622899
ZhaiD0ngxue <zhai_dongxue@foxmail.com>
2019-08-08 13:39:11 +0800

    1

--- main1.py
+++ main1.py
@@ -1,5 +1,30 @@
 
+user_name='zdx'
+user_age=18
 
-print("lets begin.")
+print("姓名",user_name,"年龄",user_age,sep='|')
+
+print(40,'\t',end='')
+print(50,'\t',end='')
+print(80,'\t')
+
+f=open('poem.txt','w')
+print('12345',file=f)
+print('asdfg',file=f)
+
+import keyword
+
+print(keyword.kwlist)
+
+a=234
+print(a)
+print(type(a))
+a=999999999999999999999999999999999999999999999
+print(a)
+print(type(a))
+a=None
+print(a)
+print(type(a))
+
 
 
