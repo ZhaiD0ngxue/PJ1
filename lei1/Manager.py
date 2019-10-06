5da9eda065ba2bd148479bfe3c1ae610d26d7708
ZESUS <zhai_dongxue@foxmail.com>
2019-10-07 01:52:56 +0800

    1

--- lei1/Manager.py
+++ lei1/Manager.py
@@ -2,9 +2,7 @@ from lei1.Customer import Customer
 from lei1.Employee import Employee
 
 class Manager(Customer,Employee):
-    def __init__(self,fa,add,sala):
-        # super().__init__(fa,add)
-        # super(Manager,self).__init__(fa,add)
-        # super(Employee,self).__init__(sala)
+    def __init__(self,fa,add,sala,age):
         Customer.__init__(self,fa,add)
         Employee.__init__(self,sala)
+        self.age=age
--- lei1/main.py
+++ lei1/main.py
@@ -1,5 +1,6 @@
 from lei1.Manager import Manager
 
-m = Manager('adf','sdf',2500)
+m = Manager('adf','sdf',2500,18)
 print(m.salary)
 m.info()
+print(m.age)
