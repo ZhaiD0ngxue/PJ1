4756fad9c1b2d57d95eedf5823778bfc29164d7d
ZESUS <zhai_dongxue@foxmail.com>
2019-10-22 00:16:23 +0800

    1

--- leiDongtai/main.py
+++ leiDongtai/main.py
@@ -1,1 +1,18 @@
 
+class Convas:
+    def draw_pci(self,shape):
+        print('---kaishi drawing----')
+        shape.draw(self)
+
+class cycle:
+    def draw(self,canvas):
+        print('cycle')
+
+class squar:
+    def draw(self,canvas):
+        print('squar')
+
+c=Convas()
+c.draw_pci(cycle())
+
+
---
+++ old/check_type.py
@@ -0,0 +1,5 @@
+
+hello='Hello'
+print(isinstance(hello,str))
+print(isinstance(hello,object))
+print(isinstance(str,object))
