09a916eccdc8ec0b0ef3f57498205342b478c7fa
Lenovo <zhai_dongxue@foxmail.com>
2019-11-18 13:58:22 +0800

    1

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #study
 print("hello world")
+age = int(input("please input your age:"))
+if age >=25:
+    print("chengnianle")
+elif age <=18:
+    print("too old")
+else:
+    print("weichengnian")
 
