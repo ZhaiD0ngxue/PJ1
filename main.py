ed3cbdd7f5ff26fa8ff96d0f12b60f1fee42ec75
Lenovo <zhai_dongxue@foxmail.com>
2020-01-13 15:54:34 +0800

    1

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
 
-import urllib.request,re
-
-def get_html(city,year,month):
-    url='https://.tianqi.com/lishi/%s/%s%s.html' % (city,year,month)
-    request = urllib.request.Request(url)
-    request.add_header('User-Agent','Mozilla/5.0')
-    return urllib.request.urlopen(request).read().decode('UTF-8')
-
-print(get_html("guangzhou",'2018','01'))
+print("hello world")
